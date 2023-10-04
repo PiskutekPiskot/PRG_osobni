6e9f63916ab7fef9ca88f9a56f439c9f9a4e4db9
Krystof Fric <krystoffric05@gmail.com>
2023-10-04 11:03:49 +0200

    RPS done

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -52,10 +54,44 @@ namespace RockPaperScissors
              * - informuj uzivatele, jake mel skore on/a a pocitac a kdo vyhral.
              */
 
-            Random rng = new Random(); //instance tridy Random pro generovani nahodnych cisel
-
-
-
+            Random rnd = new Random(); //instance tridy Random pro generovani nahodnych cisel
+            Console.WriteLine("How many rounds do you want to play?");
+            int rounds = Convert.ToInt32(Console.ReadLine());
+            int playerPoints = 0;
+            int computerPoints = 0;
+            string computerTurn = "";
+            string input = "";
+            List<string> RPS = new List<string>() {"rock","paper","scissors"};
+            for (int i = 0; i < rounds; i++)
+            {
+                Console.WriteLine("enter: rock, paper, scissors");
+                while (true)
+                { 
+                    input = Console.ReadLine().ToLower();
+                    if (RPS.Contains(input)) {break;}
+                    else { Console.WriteLine("Wrong input, try again."); } 
+                }
+                computerTurn = RPS[rnd.Next(3)];
+                if (computerTurn == input)
+                {
+                    Console.WriteLine("Computer chose " + computerTurn + " You TIED!");
+                }    
+               else
+                {
+                    if (computerTurn=="rock" && input=="paper" || computerTurn == "paper" && input == "scissors" || computerTurn == "scissors" && input == "rock")
+                    {
+                        Console.WriteLine("Computer chose "+computerTurn+" You WON!");
+                        playerPoints++;
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Computer chose " + computerTurn + " You LOST!");
+                        computerPoints++; 
+                    }
+                    
+                }
+            }
+            Console.WriteLine("You won " + playerPoints + " times and the computer won " + computerPoints + " times.");
             Console.ReadKey(); //Aby se nam to hnedka neukoncilo
         }
     }
